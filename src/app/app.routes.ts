b69a1318bc0b762959f2b97a9676c3ea27bc4cec
sohaima-siddiq <anassiddiq1984@gmail.com>
2025-12-23 13:43:39 +0500

    fix navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,10 +7,10 @@ import { Services } from './pages/services/services';
 import { Career } from './pages/career/career';
 
 export const routes: Routes = [
-    {path:'', component: Home},
-    {path:'pages/about', component:About},
-    {path:'pages/services', component:Services},
-    {path:'pages/portfolio', component:Portfolio},
-    {path: 'pages/career', component:Career},
-    {path: 'pages/contact', component:Contact}
+    {path:'', component: Home, title: 'Home'},
+    {path:'pages/about', component:About, title: 'About-Us'},
+    {path:'pages/services', component:Services, title: 'Services-Us'},
+    {path:'pages/portfolio', component:Portfolio, title: 'Portfolio'},
+    {path: 'pages/career', component:Career, title: 'Career'},
+    {path: 'pages/contact', component:Contact, title: 'Contact-Us'}
 ];
