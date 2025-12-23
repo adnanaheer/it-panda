40b2496febb8ffd7b1a56591bec3a96ba7972445
sohaima-siddiq <anassiddiq1984@gmail.com>
2025-12-23 13:19:11 +0500

    create home page and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
+import { Portfolio } from './pages/portfolio/portfolio';
+import { Services } from './pages/services/services';
+import { Career } from './pages/career/career';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: Home},
+    {path:'pages/about', component:About},
+    {path:'pages/services', component:Services},
+    {path:'pages/portfolio', component:Portfolio},
+    {path: 'pages/career', component:Career},
+    {path: 'pages/contact', component:Contact}
+];
---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, OnDestroy} from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home implements OnInit, OnDestroy {
+
+  words: string[] = ['Reality', 'Success', 'Innovation', 'Growth', 'Excellence', 'Impact', 'Achievement', 'Results'];
+  activeWord: string = '';
+
+  private wordIndex = 0;
+  private charIndex = 0;
+  private typingInterval: any;
+  private deleting = false;
+
+  ngOnInit(): void {
+    this.startTyping();
+  }
+
+  startTyping() {
+    this.typingInterval = setInterval(() => {
+
+      const currentWord = this.words[this.wordIndex];
+
+      if (!this.deleting) {
+        // typing letters
+        this.activeWord = currentWord.slice(0, this.charIndex + 1);
+        this.charIndex++;
+
+        if (this.charIndex === currentWord.length) {
+          setTimeout(() => (this.deleting = true), 800);
+        }
+      } else {
+        // deleting letters
+        this.activeWord = currentWord.slice(0, this.charIndex - 1);
+        this.charIndex--;
+
+        if (this.charIndex === 0) {
+          this.deleting = false;
+          this.wordIndex = (this.wordIndex + 1) % this.words.length;
+        }
+      }
+
+    }, 120); // typing speed
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.typingInterval);
+  }
+}
