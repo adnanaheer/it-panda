e80bd852a5fdb021626c08b5a0fe03344dcc3c78
sohaima-siddiq <anassiddiq1984@gmail.com>
2025-12-30 13:22:55 +0500

    create seo page

--- src/app/pages/services/seo/seo.ts
+++ src/app/pages/services/seo/seo.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-seo',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './seo.html',
   styleUrl: './seo.css',
 })
