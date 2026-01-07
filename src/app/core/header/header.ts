1cf1756f29237eed51c58e1ff19e698577dac322
sohaima-siddiq <anassiddiq1984@gmail.com>
2026-01-07 12:30:31 +0500

    fix

--- src/app/core/header/header.ts
+++ src/app/core/header/header.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink,RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink,RouterModule],
+  imports: [RouterLink,RouterModule,CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
 
-
+mobileOpen = false;
   
 }
